0c7fac687434fdb22d0eac9dc558bed179fcc984
kevinthlee <tae.ho@me.com>
2015-05-26 14:37:53 +0900

    Create LearnScreen_e.java

---
+++ LearnScreen_e.java
@@ -0,0 +1,245 @@
+package UI;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import leaf.WordBook;
+
+//import TOP.WordBook;
+
+public class LearnScreen_e extends JPanel implements ActionListener {
+
+	// JTextArea t;
+	// TextArea w;
+
+	JLabel titleLabel;
+	
+	JPanel mPanel;
+	JPanel titlePanel;
+	JPanel lPanel;
+	JPanel rPanel;
+	JPanel footPanel;
+
+	JButton tagButton;
+	JButton testButton;
+	JButton addButton;
+	JButton exitButton;
+	JButton back;
+	JButton exit;
+	JButton deletButton;
+
+	WordBook wordBook;
+
+	WordAddScreen wordAddScreen;
+
+	TestChoiceScreen testChoice;
+	TestScreen testScreen;
+
+	JScrollPane wordView;
+	JTable wordViewTable;
+	DefaultTableModel defaultTableModel;
+	JComboBox wordsBookList;
+	JButton wordsBookOK;
+
+	ImageIcon icon;
+
+	public LearnScreen_e() throws IOException {
+		icon = new ImageIcon("background.png");
+
+		// mPanel = new JPanel();
+		wordBook = new WordBook();
+		setLayout(new BorderLayout());
+	// mPanel.setLayout(new BorderLayout());
+
+		titlePanel = new JPanel();
+		titlePanel.setBackground(Color.BLACK);
+		titleLabel = new JLabel(new ImageIcon("img_menu/learning.png"));
+		titleLabel.setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.BOLD, 25));
+		titleLabel.setForeground(Color.WHITE);
+		titlePanel.add(titleLabel);
+		
+		add(titlePanel, BorderLayout.NORTH);
+
+		// mPanel.add(titlePanel,BorderLayout.NORTH);
+
+		lPanel = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+		
+		lPanel.setLayout(null);
+		lPanel.setBackground(Color.blue);
+
+		rPanel = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+		rPanel.setBackground(Color.orange);
+		rPanel.setLayout(null);
+		rPanel.setPreferredSize(new Dimension(190, 50));
+
+		back = new JButton(new ImageIcon("img_menu/backbutton.png"));
+		exit = new JButton(new ImageIcon("img_menu/exit.png"));
+
+		lPanel.add(back);
+		rPanel.add(exit);
+
+		back.setSize(30, 30);
+		back.setLocation(20, 10);
+
+		exit.setSize(30, 30);
+		exit.setLocation(150, 10);
+
+		// t = new JTextArea("");
+		// w = new TextArea("");
+		// JScrollPane scroll = new JScrollPane(t);
+		// scroll.setLocation(20, 180);
+		// w.setSize(300,300);
+		// w.setLocation(50, 50);
+		// lPanel.add(w);
+
+		wordsBookList = new JComboBox(WordBook.wordBookList());
+		wordsBookList.setSize(100, 30);
+		wordsBookList.setLocation(0, 50);
+
+		wordsBookOK = new JButton("º±≈√");
+		wordsBookOK.setSize(60, 30);
+		wordsBookOK.setLocation(130, 50);
+
+		rPanel.add(wordsBookOK);
+		rPanel.add(wordsBookList);
+		// footPanel.add(wordsBookOK);
+		wordsBookOK.addActionListener(this);
+
+		wordBook.loadWords("wordBooks/"
+				+ wordsBookList.getSelectedItem().toString());
+		defaultTableModel = new DefaultTableModel(wordBook.getRowData(),
+				wordBook.getColnames());
+		wordViewTable = new JTable(defaultTableModel);
+		wordViewTable.setAutoCreateRowSorter(true);
+		TableRowSorter sorter = new TableRowSorter(wordViewTable.getModel());
+		wordViewTable.setRowSorter(sorter);
+		wordView = new JScrollPane(wordViewTable);
+		wordView.setSize(350, 480);
+		wordView.setLocation(25, 50);
+		lPanel.add(wordView);
+
+		add(lPanel, BorderLayout.CENTER);
+
+		// mPanel.add(lPanel,BorderLayout.CENTER);
+
+		tagButton = new JButton("°Ÿ");
+		tagButton.setSize(130, 60);
+		tagButton.setLocation(20, 110);
+		/*
+		 * addButton = new JButton("√ﬂ∞°"); addButton.setSize(130,60);
+		 * addButton.setLocation(20, 300); testButton = new JButton("TEST");
+		 * testButton.setSize(130,60); testButton.setLocation(20,160);
+		 * deletButton = new JButton("ªË¡¶"); deletButton.setSize(130,60);
+		 * deletButton.setLocation(20,230); exitButton = new JButton("≥™∞°±‚");
+		 * exitButton.setSize(130,60); exitButton.setLocation(20,320);
+		 */
+
+		rPanel.add(tagButton);
+		/*
+		 * rPanel.add(addButton); rPanel.add(testButton);
+		 * rPanel.add(deletButton);
+		 */
+		// rPanel.add(exitButton);
+
+		tagButton.addActionListener(this);
+		/*
+		 * addButton.addActionListener(this);
+		 * testButton.addActionListener(this);
+		 * deletButton.addActionListener(this);
+		 */
+		// exitButton.addActionListener(this);
+
+		add(rPanel, BorderLayout.EAST);
+
+		// mPanel.add(rPanel,BorderLayout.EAST);
+
+		footPanel = new JPanel();
+		footPanel.setBackground(Color.red);
+
+		back.addActionListener(this);
+		exit.addActionListener(this);
+
+		// add(footPanel,BorderLayout.SOUTH);
+
+		// mPanel.add(footPanel,BorderLayout.SOUTH);
+		// add(mPanel,BorderLayout.CENTER);
+
+	}
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(icon.getImage(), 0, 0, getWidth(), getHeight(), this);
+		setOpaque(false);
+		super.paintComponent(g);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == tagButton) {
+			if (1 <= wordViewTable.getSelectedColumnCount()) {
+				wordBook.addTag(wordBook.getWordsList().get(
+						wordViewTable.getSelectedRow()));
+				JOptionPane.showMessageDialog(this, "≈¬±◊ø° √ﬂ∞°µ«æ˙Ω¿¥œ¥Ÿ.");
+			}
+		} else if (e.getSource() == wordsBookOK) {
+			try {
+				wordBook.loadWords("wordBooks/"
+						+ wordsBookList.getSelectedItem().toString());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			defaultTableModel.setDataVector(wordBook.getRowData(),
+					wordBook.getColnames());
+
+		}/*
+		 * else if(e.getSource() == addButton){ wordAddScreen = new
+		 * WordAddScreen_e(wordBook,this);
+		 * 
+		 * 
+		 * }else if(e.getSource() == testButton){ testChoice = new
+		 * TestChoiceScreen_e();
+		 * 
+		 * if(testChoice.getTestStartBool() == true){
+		 * testChoice.setTestStartBool(false); this.setVisible(false);
+		 * 
+		 * testScreen = new TestScreen_e(); Main.getMenu().add(testScreen); }
+		 * 
+		 * }else if(e.getSource() == deletButton){ if(1 <=
+		 * wordViewTable.getSelectedColumnCount()){ String str =
+		 * wordBook.getWordsList
+		 * ().get(wordViewTable.getSelectedRow()).word.toString();
+		 * if(str.charAt(0)=='*'){
+		 * wordBook.deleteWord(this.wordsBookList.getSelectedItem
+		 * ().toString(),wordBook
+		 * .getWordsList().get(wordViewTable.getSelectedRow()));
+		 * JOptionPane.showMessageDialog(this,"±‚∫ª¥‹æÓ¿Âø°º≠ ªË¡¶µ«æ˙Ω¿¥œ¥Ÿ."); }else
+		 * JOptionPane.showMessageDialog(this,"±‚∫ª¥‹æÓ¥¬ ªË¡¶ ∫“∞°¥…¿‘¥œ¥Ÿ!!"); } }
+		 */else if (e.getSource() == back) {
+			this.setVisible(false);
+			Main.getMenu().getChoiceScreen().setVisible(true);
+
+		} else if (e.getSource() == exit) {
+			System.exit(0);
+		}
+	}
+
+	public TestChoiceScreen getTestChoiceScreen_e() {
+		return testChoice;
+	}
+}
