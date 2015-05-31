c878fb409155ea97774f94339cd181517a953765
kevinthlee <tae.ho@me.com>
2015-05-31 13:44:31 +0900

    Update MenuScreen.java

--- MenuScreen.java
+++ MenuScreen.java
@@ -48,77 +48,28 @@ public class MenuScreen extends JFrame implements ActionListener {
 	TestChoiceScreen testChoice;
 	TestScreen testScreen;
 
-	public MenuScreen() {
-	
-		/*
-		 * icon = new ImageIcon("img_menu/background.png"); setLayout(new
-		 * BorderLayout()); // setLayout(null); buttonPanel = new JPanel() {
-		 * public void paintComponent(Graphics g) { g.drawImage(icon.getImage(),
-		 * 0, 0, null); setOpaque(false); super.paintComponent(g); } };
-		 * 
-		 * 
-		 * // frame setSize(600,630);
-		 * setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); setTitle("¿ŸªÁ±Õ Main");
-		 * setLayout(new BorderLayout());
-		 * 
-		 * // frame panel framePanel = new JPanel(); framePanel.setLayout(new
-		 * BorderLayout());
-		 * 
-		 * // north panel northPanel = new JPanel();
-		 * northPanel.setBackground(Color.white);
-		 * northPanel.setPreferredSize(new Dimension(0,200));
-		 * framePanel.add(northPanel,BorderLayout.NORTH);
-		 * 
-		 * // title panel titlePanel = new JPanel();
-		 * titlePanel.setBackground(Color.white); title = new
-		 * ImageIcon("title.png"); titleLabel = new JLabel();
-		 * titleLabel.setIcon(title); titlePanel.add(titleLabel);
-		 * framePanel.add(titlePanel,BorderLayout.CENTER);
-		 * 
-		 * // menu panel menuPanel = new JPanel();
-		 * menuPanel.setBackground(Color.black);
-		 * framePanel.add(menuPanel,BorderLayout.SOUTH);
-		 * 
-		 * learning = new JButton("Learning"); wordbook = new
-		 * JButton("WordList"); result = new JButton("Graph"); program = new
-		 * JButton("Program"); help = new JButton("Help");
-		 * 
-		 * menuPanel.add(learning); menuPanel.add(wordbook);
-		 * menuPanel.add(result); menuPanel.add(program); menuPanel.add(help);
-		 * 
-		 * learning.addActionListener(this); wordbook.addActionListener(this);
-		 * result.addActionListener(this); program.addActionListener(this);
-		 * help.addActionListener(this);
-		 * 
-		 * 
-		 * exit = new JButton(new ImageIcon("img_menu/exit.png"));
-		 * exit.setSize(30, 30); exit.setLocation(550, 20);
-		 * 
-		 * 
-		 * buttonPanel.add(exit); exit.addActionListener(this);
-		 * 
-		 * 
-		 * // frameø° panel√ﬂ∞° this.add(framePanel); setResizable(false);
-		 * setLocationRelativeTo(null); setVisible(true);
-		 */
-
-		icon = new ImageIcon("Img_menu/background.png");
-
+	public MenuScreen() {	
+		
 		// frame
 		setSize(600, 630);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("¿ŸªÁ±Õ Main");
 		setLayout(new BorderLayout());
-
+		
+		
 		// frame panel
-		framePanel = new JPanel() {
-			public void paintComponent(Graphics g) {
-				g.drawImage(icon.getImage(), 0, 0, null);
-				setOpaque(false);
-				super.paintComponent(g);
-			}
-		};
-		framePanel.setLayout(new BorderLayout());
+        framePanel = new JPanel() {
+	    public void paintComponent(Graphics g) {
+		g.drawImage(icon.getImage(), 0, 0, null);
+		setOpaque(false);
+		super.paintComponent(g);
+	       }
+        };
+        framePanel.setLayout(new BorderLayout());
+
+		icon = new ImageIcon("Img_menu/main_menu.png");
+		
+		
 
 		menuPanel = new JPanel() {
 			public void paintComponent(Graphics g) {
@@ -131,30 +82,33 @@ public class MenuScreen extends JFrame implements ActionListener {
 		menuPanel.setBackground(Color.pink);
 
 		learning = new JButton(new ImageIcon("img_menu/learning.png"));
-		learning.setSize(145, 35);
-		learning.setLocation(227, 75);
+		learning.setSize(210, 60);
+		learning.setLocation(200, 200);
 
 		wordbook = new JButton(new ImageIcon("img_menu/list.png"));
-		wordbook.setSize(145, 35);
-		wordbook.setLocation(227, 185);
+		wordbook.setSize(210, 60);
+		wordbook.setLocation(200, 270);
 
 		// Graph ¥‹√ﬂ ¿⁄∏Æ
 		result = new JButton(new ImageIcon("img_menu/result.png"));
-		result.setSize(145, 35);
-		result.setLocation(227, 295);
+		result.setSize(210, 60);
+		result.setLocation(200, 340);
 
 		// Program ¥‹√ﬂ ¿⁄∏Æ
 		program = new JButton(new ImageIcon("img_menu/information.png"));
-		program.setSize(145, 35);
-		program.setLocation(227, 405);
+		program.setSize(210, 60);
+		program.setLocation(200, 410);
 
 		help = new JButton(new ImageIcon("img_menu/help.png"));
-		help.setSize(145, 35);
-		help.setLocation(227, 515);
+		help.setSize(210, 60);
+		help.setLocation(200, 480);
 		
 		exit = new JButton(new ImageIcon("img_menu/exit.png"));
-		exit.setSize(30, 30);
-		exit.setLocation(550, 20);
+		exit.setBorderPainted(false);
+		exit.setFocusPainted(false);
+		exit.setContentAreaFilled(false);
+		exit.setSize(50, 50);
+		exit.setLocation(520, 20);
 
 		// wrongWordButton = new JButton("ø¿¥‰≥Î∆Æ");
 		// wrongWordButton.setSize(200,100);
@@ -170,13 +124,13 @@ public class MenuScreen extends JFrame implements ActionListener {
 		menuPanel.add(result);
 		menuPanel.add(program);
 		menuPanel.add(help);
+		menuPanel.add(exit);
 
 		learning.addActionListener(this);
 		wordbook.addActionListener(this);
 		result.addActionListener(this);
 		program.addActionListener(this);
 		help.addActionListener(this);
-		
 		exit.addActionListener(this);
 		
 		framePanel.add(menuPanel, BorderLayout.CENTER);
@@ -238,7 +192,7 @@ public class MenuScreen extends JFrame implements ActionListener {
 				this.add(programScreen);
 				programScreen.updateUI();
 			} else {
-				resultScreen.setVisible(true);
+				programScreen.setVisible(true);
 			}
 
 		} else if (e.getSource() == help) {
