e96b1421ba65ff810018ccebd2773baaeaee69a7
kevinthlee <tae.ho@me.com>
2015-05-31 13:47:29 +0900

    Create Result.java

---
+++ Result.java
@@ -0,0 +1,19 @@
+package leaf;
+
+public class Result {
+	public String time;
+	public String score;
+	
+	public Result(String t, String s){
+		time = t;
+		score = s;
+		}
+	
+	public void settime(String t){
+		time = t;
+	}
+	
+	public void setscore(String s){
+		score = s;
+	}
+}
