ec30d61c7402c2ce1b427ed8b5558ddebabd173c
kevinthlee <tae.ho@me.com>
2015-05-31 13:48:05 +0900

    Update Word.java

--- Word.java
+++ Word.java
@@ -4,13 +4,18 @@ public class Word {
 	public String word;
 	public String mean;
 	public String level;
+	public String time;
+	public String score;
 	
 	public Word(String w, String m, String l){
 		word = w;
 		mean = m;
 		level = l;
 	}
-	
+	public Word(String t, String s){
+		time = t;
+		score = s;
+	}	
 	public void setWord(String w){
 		word = w;
 	}
