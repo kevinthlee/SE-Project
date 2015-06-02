7c1d99238c8969ca443adca0d576ebf1c1891a96
kevinthlee <tae.ho@me.com>
2015-06-02 18:43:41 +0900

    Update TestScreen.java

--- TestScreen.java
+++ TestScreen.java
@@ -21,8 +21,8 @@ import leaf.Test;
 
 public class TestScreen extends JPanel implements ActionListener{
 	ImageIcon icon;
-	JPanel titlePanel;
 	
+	//JPanel titlePanel;
 	JPanel testPanel;
 	JPanel testNorthPanel;
 	JPanel testCenterPanel;
@@ -33,6 +33,7 @@ public class TestScreen extends JPanel implements ActionListener{
 	
 	JTextField answerField;
 	JTextPane quizArea;
+	
 	JRadioButton answer1;
 	JRadioButton answer2;
 	JRadioButton answer3;
@@ -51,12 +52,10 @@ public class TestScreen extends JPanel implements ActionListener{
 	public TestScreen() {
 		
 		setLayout(new BorderLayout());
-		
 		test = new Test(Main.getMenu().getChoiceScreen().getTestChoiceScreen().getTestType(), Main.getMenu().getChoiceScreen().getTestChoiceScreen().getSelectDay(), this);
+		icon = new ImageIcon("img_menu/test_back.png");
 		
-		/*
-		icon = new ImageIcon("img_menu/learning_back.png");
-		setLayout(new BorderLayout());
+		//setLayout(new BorderLayout());
 		// setLayout(null);
 		
 		testPanel = new JPanel() {
@@ -66,46 +65,83 @@ public class TestScreen extends JPanel implements ActionListener{
 				super.paintComponent(g);
 			}
 		};
-		*/
+	
+		testPanel.setLayout(new BorderLayout());
+		testPanel.setLayout(null);
 		
 		// title Panel
-		titlePanel = new JPanel();
+		/*titlePanel = new JPanel();
 		titlePanel.setBackground(Color.BLACK);
 		titleLabel = new JLabel("Test");
 		titleLabel.setFont(new Font("∏º¿∫ ∞ÌµÒ",Font.BOLD,25));
-		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setForeground(Color.WHITE);*/
 		
-		nameLabel = new JLabel(" - "+test.getSelectedDay());
+		nameLabel = new JLabel(test.getSelectedDay());
 		nameLabel.setForeground(Color.WHITE);
-		titlePanel.add(titleLabel);
-		titlePanel.add(nameLabel);
-		titlePanel.setPreferredSize(new Dimension(0,200));
-		add(titlePanel,BorderLayout.NORTH);
+		nameLabel.setFont(new Font("∏º¿∫ ∞ÌµÒ",Font.BOLD,20));
+		nameLabel.setSize(150, 40);
+		nameLabel.setLocation(250, 130);
+		
+		testPanel.add(nameLabel);
+		
+		//testPanel.setPreferredSize(new Dimension(0,200));
+		
 		
 		// test Panel
-		testPanel = new JPanel();
-		testPanel.setLayout(new BorderLayout());
-		testSouthPanel = new JPanel();
+		
+		/*testSouthPanel = new JPanel();
 		testSouthPanel.setBackground(Color.BLACK);
 		testCenterPanel = new JPanel();
 		testNorthPanel = new JPanel();
-		testNorthPanel.setBackground(Color.BLACK);
+		testNorthPanel.setBackground(Color.BLACK);*/
+		
 		quizArea = new JTextPane();
-		quizArea.setFont(new Font("∏º¿∫ ∞ÌµÒ",Font.BOLD,40));
+		quizArea.setForeground(Color.BLACK);
+		quizArea.setBackground(null);
+		quizArea.setFont(new Font("∏º¿∫ ∞ÌµÒ",Font.BOLD,35));
 		quizArea.setEditable(false);
-		testNorthPanel.setPreferredSize(new Dimension(0,150));
-		testNorthPanel.add(quizArea);
+		quizArea.setSize(500, 120);
+		quizArea.setLocation(50, 180);
+		
+		testPanel.add(quizArea);
+		
+		//testNorthPanel.setPreferredSize(new Dimension(0,150));
+		
 		
 		if(test.getTestType() == Test.TEST_TYPE_1){
-			answerField = new JTextField(30);
-			testCenterPanel.add(answerField);
+			answerField = new JTextField(70);
+			answerField.setSize(150, 30);
+			answerField.setLocation(225, 340);
+			testPanel.add(answerField);
 		}else{
 			answer1 = new JRadioButton("");
+			answer1.setSize(200, 30);
+			answer1.setLocation(50, 310);
+			answer1.setContentAreaFilled(false);
+			
 			answer2 = new JRadioButton("");
+			answer2.setSize(200, 30);
+			answer2.setLocation(50, 350);
+			answer2.setContentAreaFilled(false);
+			
 			answer3 = new JRadioButton("");
+			answer3.setSize(200, 30);
+			answer3.setLocation(50, 390);
+			answer3.setContentAreaFilled(false);
+			
 			answer4 = new JRadioButton("");
+			answer4.setSize(200, 30);
+			answer4.setLocation(50, 430);
+			answer4.setContentAreaFilled(false);
+						
 			answer5 = new JRadioButton("");
+			answer5.setSize(200, 30);
+			answer5.setLocation(50, 470);
+			answer5.setContentAreaFilled(false);
+			
+			
 			dummyRadio = new JRadioButton();
+						
 			multipleAnswer = new ButtonGroup();
 			multipleAnswer.add(answer1);
 			multipleAnswer.add(answer2);
@@ -113,30 +149,46 @@ public class TestScreen extends JPanel implements ActionListener{
 			multipleAnswer.add(answer4);
 			multipleAnswer.add(answer5);
 			multipleAnswer.add(dummyRadio);
-			testCenterPanel.add(answer1);
-			testCenterPanel.add(answer2);
-			testCenterPanel.add(answer3);
-			testCenterPanel.add(answer4);
-			testCenterPanel.add(answer5);
+						
+			testPanel.add(answer1);
+			testPanel.add(answer2);
+			testPanel.add(answer3);
+			testPanel.add(answer4);
+			testPanel.add(answer5);
 		}
 		
-		nextButton = new JButton("¥Ÿ¿Ω");
-		testSouthPanel.add(nextButton);
+				
+		nextButton = new JButton(new ImageIcon("img_menu/check.png"));
+		
+		nextButton.setBorderPainted(false);
+		nextButton.setFocusPainted(false);
+		nextButton.setContentAreaFilled(false);
+		nextButton.setSize(50, 50);
+		nextButton.setLocation(275, 500);
+		
+		testPanel.add(nextButton);
 		nextButton.addActionListener(this);
 		
-		testPanel.add(testNorthPanel,BorderLayout.NORTH);
+		/*testPanel.add(testNorthPanel,BorderLayout.NORTH);
 		testPanel.add(testCenterPanel,BorderLayout.CENTER);
-		testPanel.add(testSouthPanel,BorderLayout.SOUTH);
+		testPanel.add(testSouthPanel,BorderLayout.SOUTH);*/
 		add(testPanel,BorderLayout.CENTER);
 		
 		// button Panel
-		buttonPanel = new JPanel();
-		buttonPanel.setBackground(Color.BLACK);
-		exit = new JButton("≥™∞°±‚");
-		exit.addActionListener(this);
-		buttonPanel.add(exit);
+		//buttonPanel = new JPanel();
+		//buttonPanel.setBackground(Color.BLACK);
 		
-		add(buttonPanel,BorderLayout.SOUTH);
+		exit = new JButton(new ImageIcon("img_menu/exit.png"));
+		exit.addActionListener(this);
+		exit.setBorderPainted(false);
+		exit.setFocusPainted(false);
+		exit.setContentAreaFilled(false);
+		exit.setSize(50, 50);
+		exit.setLocation(520, 20);
+		testPanel.add(exit);
+		
+		
+		//add(buttonPanel,BorderLayout.SOUTH);
 		
 		//Ω√«ËΩ√¿€
 		test.submitQuiz();
@@ -145,16 +197,18 @@ public class TestScreen extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		boolean check = false;
 		if(e.getSource() == exit){
 			test.endTest();
-			Main.getMenu().remove(this);
-			Main.getMenu().getFramePanel().setVisible(true);
+			this.setVisible(false);
+			//Main.getMenu().getFramePanel().setVisible(true);
 		}else if(e.getSource() == nextButton){
 			
 			test.checkAnswer();
-			test.submitQuiz();
-			
+			check = test.submitQuiz();
 		}
+		if (check)
+			this.setVisible(false);
 	}
 	
 	public JRadioButton getAnswer1(){
